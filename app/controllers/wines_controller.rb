b9c951887276a5984730350fa3342e895df000b8
Paige Murphy <pmurph11@gmu.edu>
2014-04-05 16:03:16 -0400

    Adding Homework 9

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,9 +1,58 @@
 class WinesController < ApplicationController
-	def index
-		@wines = Wine.all
-	end
+#	def index
+#		@wines = Wine.all
+#	end
 	
-	def show
-		@wine = Wine.find(params[:id])
-	end
+#	def show
+#		@wine = Wine.find(params[:id])
+#	end
+before_action :set_wine, only: [ :show, :edit, :update, :destroy ]
+
+
+def index
+@wines = Wine.all
+end
+
+def show
+   #This is empty now @wine = Wine.find(params[:id])
+end
+
+def new
+@wine = Wine.new
+end
+
+def create
+@wine = Wine.new(wine_params)
+@wine.save
+redirect_to @wine
+end
+
+def edit
+   #This is empty now @wine = Wine.find(params[:id])
+end
+
+
+def update
+#This is empty now @wine = Wine.find(params[:id])
+@wine.update(wine_params)
+redirect_to @wine
+end
+
+def destroy
+#This is empty now @wine = Wine.find(params[:id])
+@wine.destroy
+redirect_to wines_url
+end
+ 
+
+private
+
+def wine_params
+params.require(:wine).permit(:name, :year, :winery, :country, :varietal)
+end
+
+def set_wine
+@wine = Wine.find(params[:id])
+end
+ 
 end
