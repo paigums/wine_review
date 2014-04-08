03a4322fce4e4aa66fc9b688fc44419de987eba4
Student <pmurph11@gmu.edu>
2014-04-08 19:12:45 -0400

    Adding Homework - Validating Models

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,2 +1,9 @@
 class Wine < ActiveRecord::Base
+	validates :name, :year, :winery, :country, :varietal, presence: true
+validates :year,
+   numericality: { only_integer: true, greater_than_or_equal_to: 0 },
+   unless: "year.blank?"
+validates :name, 
+    numericality: { greater_than_or_equal_to: 0 },
+    if: "name.present?"
 end
