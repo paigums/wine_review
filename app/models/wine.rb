8318eba45789297f32514bc6a209c1a9fbd5e4e8
Paige Murphy <pmurph11@gmu.edu>
2014-04-15 19:26:26 -0400

    Association Calculations

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -10,4 +10,10 @@ class Wine < ActiveRecord::Base
    	validates :price, 
     numericality: { greater_than_or_equal_to: 0 },
     if: "price.present?"
+
+    	
+def average_stars
+  reviews.average(:stars)
+end
+
 end
